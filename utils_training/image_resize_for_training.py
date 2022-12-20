535c2e41221ee2df778a7be0c09b169cd5f89e65
Nina <nninanikitina@gmail.com>
2022-12-20 12:45:40 -0700

    update resize script

--- utils_training/image_resize_for_training.py
+++ utils_training/image_resize_for_training.py
@@ -3,15 +3,26 @@ import cv2.cv2 as cv2
 import os
 
 if __name__ == '__main__':
-    input_folder = r"C:\Users\nnina\Desktop\mask_max_pr"
-    output_folder = r"C:\Users\nnina\Desktop\512_mask_max_pr"
+    input_folder_img = r"D:\BioLab\img\training_sets\Nucleus_training_img_and_masks\nucleus_training_mathlab_alternative\Anamaris stiched training set\imgs_2048"
+    output_folder_img = r"D:\BioLab\img\training_sets\Nucleus_training_img_and_masks\nucleus_training_mathlab_alternative\Anamaris stiched training set\imgs_512"
 
-    for img_path in glob.glob(os.path.join(input_folder, "*.bmp")):
+    input_folder_mask = r"D:\BioLab\img\training_sets\Nucleus_training_img_and_masks\nucleus_training_mathlab_alternative\Anamaris stiched training set\masks_2048"
+    output_folder_mask = r"D:\BioLab\img\training_sets\Nucleus_training_img_and_masks\nucleus_training_mathlab_alternative\Anamaris stiched training set\masks_512"
+
+    for img_path in glob.glob(os.path.join(input_folder_img, "*.png")):
         img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-        name_1 = os.path.split(img_path)[1].split("_")[0]
-        name_2 = os.path.split(img_path)[1].split(" ")[1].split("_")[-1]
+        name_1 = os.path.split(img_path)[1].split(".")[0]
         resized_img = cv2.resize(img.astype('uint8'), (512, 512), interpolation=cv2.INTER_AREA)
 
-        output_img_path = os.path.join(output_folder, name_1 + "_" + name_2 + ".bmp")
+        output_img_path = os.path.join(output_folder_img, name_1 + ".png")
         cv2.imwrite(output_img_path, resized_img)
         print(output_img_path)
+
+    for img_path in glob.glob(os.path.join(input_folder_mask, "*.bmp")):
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        name_1 = os.path.split(img_path)[1].split(".")[0]
+        resized_mask = cv2.resize(img.astype('uint8'), (512, 512), interpolation=cv2.INTER_AREA)
+
+        output_mask_path = os.path.join(output_folder_mask, name_1 + ".bmp")
+        cv2.imwrite(output_mask_path, resized_mask)
+        print(output_mask_path)
